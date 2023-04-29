f673e5d8c04f1c9ab099f224588543f621f5f382
Paulo Riveiro <pauloriveiro01@gmail.com>
2023-04-29 19:52:20 +0000

    v1 not finsihed

---
+++ src/js/component/toDolist.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../../styles/index.css'
+
+export const ToDoList = () => {
+
+    
+
+
+
+    return (<>
+        <div className="container">
+            <input type="text" className="field"/>
+            <ul class="list-group">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+            </ul>
+        </div>
+        </>)
+
+}
